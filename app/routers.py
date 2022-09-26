d07ee156ffe72c243385eedf8c6558555aee41ac
Максим Скибин <skibin-mvp@mail.ru>
2022-09-27 00:10:56 +0300

    routes fixes due to bd refactor

--- app/routers.py
+++ app/routers.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.articles_base import articles_bd, Art
+from app.articles_base import ArticlesDB, Art
 from app.topic_model import TopicName
 
 
 router = APIRouter()
+articles_bd = ArticlesDB()
 
 
 @router.get('/')
@@ -15,20 +16,21 @@ def read_root():
             '/articles',
             '/articles/<str art_topic> (Web, IT, NN)',
             '/articles/<str art_topic>?number=<int number>',
-            '/articles/add in docs']
+            '/articles/add in docs',
+            '/cgeck_topic/{topic_name} in docs']
         ]
     return content
 
 
 @router.get('/articles')
 async def get_aricles():
-    articles = [art['title'] for art in articles_bd]
+    articles = [art['title'] for art in articles_bd.content]
     return articles
 
 
 @router.get('/articles/{art_topic}')
 async def get_certain_aricles(art_topic: str, number: int | None = None):
-    articles = [art['title'] for art in articles_bd if art['topic'] == art_topic]
+    articles = [art['title'] for art in articles_bd.content if art['topic'] == art_topic]
     return articles[:number]
 
 
@@ -37,12 +39,14 @@ async def add_article(art: Art):
     article = art.dict()
     if art.published is not None:
         article['published'] = art.published
-    return article
+    articles_bd.add_art(article)
+    text = 'Article was successfully published!'
+    return text
 
 @router.get('/check_topic/{topic_name}')
 async def check_topic(topic_name: TopicName):
     if topic_name is TopicName.web:
-        n = len(list(filter(lambda art: art['topic'] == TopicName.web.value, articles_bd)))
+        n = len(list(filter(lambda art: art['topic'] == TopicName.web.value, articles_bd.content)))
         return {'info': f'There\'s {n} articles of {TopicName.web.value} topic!'}
     if topic_name.value == 'IT':
         return {'topic_name': topic_name, 'info': 'I love IT!'}
