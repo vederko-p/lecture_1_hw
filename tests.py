413cef55c609c07a3c3820f1ecf0ddb8fb2f17e1
Максим Скибин <skibin-mvp@mail.ru>
2022-09-27 17:10:46 +0300

    integration test added

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from app.utils import check_bd_free_space, check_originality
-from app.articles_base import ArticlesDB
+from app.articles_base import ArticlesDB, Art
 
 
 def test_check_bd():
@@ -12,3 +12,15 @@ def test_check_originality_type():
     res = check_originality(1, 1)
     assert isinstance(res, tuple)
 
+
+def test_integration():
+    articles_bd = ArticlesDB()
+    article_new = {
+        'title': 'New Title with Neural Network',
+        'topic': 'NN',
+        'published': 137
+    }
+    len_before = articles_bd.len
+    articles_bd.add_art(article_new)
+    len_after = articles_bd.len
+    assert len_after - len_before == 1
